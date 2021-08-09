2f09f66835473aa69e77eda3d9a11d4e9214f83e
vmstr <marcroesner@googlemail.com>
2021-08-09 23:28:33 +0200

    New title

--- controllers/alien-shooter-main-code.js
+++ controllers/alien-shooter-main-code.js
@@ -11,6 +11,127 @@ pixelMap.set('cover', {
   y: gameWindow.height + gameWindow.height,
   sizeFactor: 1,
 });
+
+const eusaSysiphus = {
+  "E": [
+    [0,1,1,1],
+    [1,0,0,0],
+    [1,1,1,0],
+    [1,0,0,0],
+    [0,1,1,1], 
+  ],
+  "U": [
+    [1,0,0,1],
+    [1,0,0,1],
+    [1,0,0,1],
+    [1,0,0,0],
+    [0,1,1,0], 
+  ],
+  "S": [
+    [0,1,1,1],
+    [1,0,0,0],
+    [0,1,1,1],
+    [0,0,0,1],
+    [1,1,1,0], 
+  ],
+  "S": [
+    [0,1,1,1],
+    [1,0,0,0],
+    [0,1,1,1],
+    [0,0,0,1],
+    [1,1,1,0],
+  ],
+  "S": [
+    [0,1,1,1],
+    [1,0,0,0],
+    [0,1,1,1],
+    [0,0,0,1],
+    [1,1,1,0], 
+  ],
+  "Y": [
+    [1,0,0,1],
+    [1,0,0,1],
+    [0,1,1,1],
+    [0,0,0,1],
+    [0,0,0,1], 
+  ],
+  "S": [
+    [0,1,1,1],
+    [1,0,0,0],
+    [0,1,1,1],
+    [0,0,0,1],
+    [1,1,1,0], 
+  ],
+  "I": [
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0],
+    [0,1,0,0], 
+  ],
+  "P": [
+    [0,1,1,1],
+    [1,0,0,1],
+    [1,1,1,1],
+    [1,0,0,0],
+    [1,0,0,0], 
+  ],
+  "H": [
+    [1,0,0,1],
+    [1,0,0,1],
+    [1,1,1,1],
+    [1,0,0,1],
+    [1,0,0,1], 
+  ],
+  "U": [
+    [1,0,0,1],
+    [1,0,0,1],
+    [1,0,0,1],
+    [1,0,0,1],
+    [0,1,1,0], 
+  ],
+  "S": [
+    [0,1,1,1],
+    [1,0,0,0],
+    [0,1,1,1],
+    [0,0,0,1],
+    [1,1,1,0], 
+  ],
+  "2": [
+    [1,1,1,0],
+    [0,0,0,1],
+    [0,1,1,0],
+    [1,0,0,0],
+    [0,1,1,1], 
+  ],
+ }
+
+ function draw (x, y, size, arr, key, canvas, c1, c2, c3) {
+	let firstX = x;
+  for ( let i = 0; i <= arr[key].length-1; i+=1 ) {
+    x = firstX;
+    y += size;
+    for ( let j = 0; j <= arr[key].length-2; j+=1 ) {
+      x = x + size;
+      color = '#' + Math.round(c1) + Math.round(c2) + Math.round(c3);
+      if ( arr[key][i][j] === 0 ) color = '#000';
+      rect(x, y, size, color, canvas);
+    }	
+  }
+}
+
+function rect(x, y, size, color, canvas) {
+	canvas.fillStyle = color;
+  canvas.fillRect(x,y,size,size);
+}
+
+let x = 25, y = 25;
+let start_y = y;
+let size = 5;
+let color;
+let dir = 1;
+let c1 = 0, c2 = 1, c3 = 2;
+
 function titleScreen() {
   let highscore = document.cookie.split(';').filter((item) => item.trim().startsWith('highscore='));
   console.log(highscore[0]);
@@ -31,12 +152,49 @@ function titleScreen() {
   // console.log(pixelMap.get('cover').sizeFactor);
   display.drawImage(coverFigure, pixelMap.get('cover').x, pixelMap.get('cover').y, gameWindow.width / 20 * ( pixelMap.get('cover').sizeFactor), gameWindow.height / 20 * ( pixelMap.get('cover').sizeFactor ));
   display.fillStyle = 'white';
-  display.font = '14px Consolas';
-  display.fillText('Aliens are attacking earth. Don\'t let them through!', gameWindow.width * 0.05, gameWindow.height * 0.1);
+  display.font = '12px Consolas';
+  display.fillText('Aliens are attacking earth. Don\'t let them through!', gameWindow.width * 0.05, gameWindow.height * ( 0.89 + 0.05 ));
   display.font = '12px Consolas';
   display.fillText('[ENTER] START', gameWindow.width * 0.75, gameWindow.height * 0.86);
   display.fillText('[F5] Restart [F11] FULL Screen', gameWindow.width * 0.75, gameWindow.height * 0.89);
   display.fillText('Highscore: ' + highscore, gameWindow.width * 0.05, gameWindow.height * 0.89);
+  c1 += 0.05;
+  c2 += 0.05;
+  c3 += 0.05;
+  x = gameWindow.width * 0.05;
+  y = gameWindow.height * 0.1;
+  draw(x, y, size, eusaSysiphus, 'E', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'U', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'S', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'S', display, c1, c2, c3);
+  x = x + size * 10;
+  draw(x, y, size, eusaSysiphus, 'S', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'Y', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'S', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'I', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'P', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'H', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'U', display, c1, c2, c3);
+  x = x + size * 5;
+  draw(x, y, size, eusaSysiphus, 'S', display, c1, c2, c3);
+  x = x + size * 20;
+  draw(x, y, size, eusaSysiphus, '2', display, c1, c2, c3);
+  if (c1 >= 9) c1 = 0;
+  if (c2 >= 9) c2 = 0;
+  if (c3 >= 9) c3 = 0;
+  size = size + 0.005 * dir;
+  if (Math.round(size) == 10) { dir = -1; } 
+  if (Math.round(size, 1) == 8) { dir = 1; };
+  console.log(size);
 }
 
 let activeKeys = {};
